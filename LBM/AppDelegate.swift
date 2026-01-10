9938cc760e4242e16dae6eec29b9fd29992d15ea
pjoly <59795845+Pierre-Joly@users.noreply.github.com>
2026-01-10 19:47:45 +0100

    Initial Commit

---
+++ LBM/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  LBM
+//
+//  Created by Pierre joly on 10/01/2026.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+}
